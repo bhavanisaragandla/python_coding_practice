3eb5fbc459606edc56b8c8e90db45efa8e038de2
Bhavani <bhavanisaragandla@gmail.com>
2026-08-14 11:50:30 +0530

    problem2.py

---
+++ Python-basics/problem2.py
@@ -0,0 +1,48 @@
+# Reverse a string 
+'''text = input("Enter a string:")
+reverse = text[::-1]
+print("Reverse string:",reverse)'''
+
+# count digits in a number
+'''n = int(input("Enter a number:"))
+count = 0
+while n > 0:
+    n//=10
+    count+=1
+print("Count :",count) '''   
+
+# sum of digits in a number
+'''n = int(input("Enter a number:"))
+sum = 0
+while n > 0:
+    digit = n % 10
+    sum += digit
+    n//=10
+print("Sum of digits:",sum)  '''  
+
+# check whether a number is a palindrome or not
+'''n = int(input("Enter n value:"))
+original = n
+reverse = 0
+while n > 0 :
+    digit = n % 10
+    reverse = reverse * 10 + digit
+    n //= 10
+if original == reverse:
+    print("Palindrome")
+else:
+    print("Not Palindrome")  '''  
+
+# Argstrong Number
+n = int(input("Enter a number"))
+original = n
+digits=len(str(n))
+total = 0
+while n > 0:
+    digit = n %10
+    total += digit ** digits
+    n //= 10
+if total == original :
+    print("Argstone")
+else:
+    print("Not Argstone")
